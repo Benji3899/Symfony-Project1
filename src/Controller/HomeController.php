6a456cd34969d4dce9eba065b5987439b1f638bd
Benjamin EPS <benjamin_eps@hotmail.fr>
2023-05-07 11:00:10 +0200

    Route variable

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,44 @@
+<?php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Config\Doctrine\Dbal\ConnectionConfig\ReplicaConfig;
+use function Symfony\Component\String\u;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', 'home', methods: ['GET'])]
+    public function index(): Response
+    {
+        $prenoms = ['jean-pierre', 'tom', 'bob'];
+
+        return $this->render('home.html.twig', [
+            'prenoms' => $prenoms
+        ]);
+    }
+
+    //Route variable grâce à la connection entre {name} et $name
+    // Par défaut le chemin avant {e} n'est pas accessible dans l'URL. Donc je ne peux pas me rendre à /hello
+    // Pour résoudre ce problème je dois rendre {e} facultatif en
+    // ajoutant = null dans les paramètres de la function hello($name)
+    #[Route('/hello/{name}', name: 'toto' )]
+    public function hello($name = null): Response
+    {
+        return $this->render('hello.html.twig', [
+            'name' => $name
+        ]);
+    }
+
+    #[Route('/browse/{slug}')]
+    public function browse(string $slug = null): Response
+    {
+        if($slug) {
+            $title = 'Genre: ' . u(str_replace('-', ' ', $slug))->title(true);
+        }else{
+            $title = 'All genre';
+        }
+        return new Response($title);
+    }
+}
